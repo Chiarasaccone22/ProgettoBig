8d44482b027637b524c05d5237b9dd46ce20f42a
MatteoIng <matteofoschi10@gmail.com>
2023-08-19 17:19:13 +0200

    RISOLTO PROBLEMA DI CHIARAAA

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
+from waitress import serve
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT']=0
+
 @app.route('/')
-
 #funzione che gestisce la home page
-
 def index():
 #per il tamplate html
     return render_template('index.html')
 
 if __name__ == "__main__":
-    from waitress import serve
+    app.run(debug=True)
     serve(app, host="0.0.0.0", port=8080)
