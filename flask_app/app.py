49a4572642e85fe52997b9c971f24b8a4362ac53
chiara saccone <chi.saccone@stud.uniroma3.it>
2023-08-19 18:10:10 +0200

    hai fatto tu

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from waitress import serve
+import sqlite3
+import psycopg2
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT']=0
@@ -7,8 +9,40 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT']=0
 @app.route('/')
 #funzione che gestisce la home page
 def index():
+#connessione tra database e pagina web
+    connection=sqlite3.connect('database.db')
+    connection.row_factory= sqlite3.Row #ho messo .row perchè la connessione al database è organizzata in righe e infatti il nostro database è fatto dalle ennuple
+    posts=connection.execute('SELECT * FROM posts').fetchall() #.fetchall fa in modo che le ennuple siano organizzate in una lista python semplice
+    connection.close()
+
+#ora che abbiamo la lista python di posts, che sarebbero le ennuple del nostro database, dobbiamo passare la lista posts nel template html per il collegamento
+#per aggiungere un qualsiasi oggetto e passarlo al tamplate html basta passarlo come argomento della funzione render_template : posts=posts dove il primo è il nome con cui lo accediamo in html e il secondo è ciò che abbiamo creato qui
 #per il tamplate html
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
+
+@app.route('/connectionpostgres')
+def connection():
+   
+
+
+    conn = psycopg2.connect(
+    host="localhost",
+    port="5432",
+    user="postgres",
+    password="password",
+    database="postgres"
+)
+
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE posts ( titolo TEXT, info TEXT)")
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    conn.close()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ flask_app/crea_posts.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+    titolo TEXT,
+    info TEXT
+);
+
+INSERT INTO posts(titolo, info) VALUES ('Lava il gatto', 'Lucidare accuratamente il pelo');
+INSERT INTO posts(titolo, info) VALUES ('mangia il cane', 'Lucidare accuratamente il peloooooooooooo');
+INSERT INTO posts(titolo, info) VALUES ('vulcano', 'scappa');
+INSERT INTO posts(titolo, info) VALUES ('mucca', 'dai da mangiare');
