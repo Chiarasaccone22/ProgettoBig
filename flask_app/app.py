8639fce3656dbe52c20b3d7e0d7d16423442bdb2
chiara saccone <chi.saccone@stud.uniroma3.it>
2023-08-21 11:09:01 +0200

    connessione poatgres

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,20 +1,30 @@
 from flask import Flask, render_template
 from waitress import serve
-import sqlite3
 import psycopg2
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT']=0
 
 @app.route('/')
-#funzione che gestisce la home page
 def index():
-#connessione tra database e pagina web
-#ora che abbiamo la lista python di posts, che sarebbero le ennuple del nostro database, dobbiamo passare la lista posts nel template html per il collegamento
-#per aggiungere un qualsiasi oggetto e passarlo al tamplate html basta passarlo come argomento della funzione render_template : posts=posts dove il primo è il nome con cui lo accediamo in html e il secondo è ciò che abbiamo creato qui
-#per il tamplate html
-    return render_template('index.html')
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port="5432",
+        user="postgres",
+        password="password",
+        database="postgres"
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM conti")
+    results = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return render_template('index.html', posts=results)
 
 if __name__ == "__main__":
-    #app.run(debug=True)
+    app.run(debug=True)
     serve(app, host="0.0.0.0", port=8080)
