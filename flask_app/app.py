d5ece801666606cdf209d9627e011fc040f20bba
chiara saccone <chi.saccone@stud.uniroma3.it>
2023-08-21 12:52:54 +0200

    connessioni che vanno bene con: postgres, dynamo e mongo

--- flask_app/app.py
+++ flask_app/app.py
@@ -8,14 +8,14 @@ import pymongo
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT']=0
 
-@app.route('/connPostgres')
+#apertura di default
+@app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/connPostgres')
 def connPostgres():
 
-    print('connessione...')
     conn = psycopg2.connect(
         host="172.22.0.6",
         port="5432",
@@ -23,41 +23,32 @@ def connPostgres():
         password="password",
         database="postgres"
     )
-    print('connesso...')
+
     cursor = conn.cursor()
-    print('faccio query...')
     cursor.execute("SELECT * FROM posts")
     results = cursor.fetchall()
-    print('chiudo connessione...')
     cursor.close()
     conn.close()
 
     return render_template('index.html', posts=results)
 
-@app.route('/')
+@app.route('/connDynamo')
 #funzione che gestisce la home page
 def connDynamo():
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('Books')
-    #Esegui una query sulla tabella utilizzando il titolo come chiave
-    response = table.query(
-        KeyConditionExpression= "book_id= :book_id",
-        ExpressionAttributeValues= {
-            ":book_id": {"N": "3"}
-            } 
-)
+    tables = list(dynamodb.tables.all())
+  
 
-
-
-    items = response['Items']
-
-    return render_template('index.html', posts=items)
+    return render_template('index.html', posts=tables)
 
 @app.route('/connMongo')
 def connMongo():
     connessione = pymongo.MongoClient("mongodb://172.22.0.2:27017/")
     l = connessione.list_database_names()
     return render_template('index.html',posts=l)
+
+
+
 
 if __name__ == "__main__":
     #app.run(debug=True)
