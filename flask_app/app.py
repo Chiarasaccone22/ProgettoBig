b916fc069936d984d09a21a20a28bf3a203a3154
chiara saccone <chi.saccone@stud.uniroma3.it>
2023-08-23 11:24:17 +0200

    Connessioni finite tutto ok yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- flask_app/app.py
+++ flask_app/app.py
@@ -61,12 +61,11 @@ def connNeo():
 def connCassandra():
     cluster = Cluster(['cassandraDb'], port=9042)
     session = cluster.connect()
-    session.execute("""
-    CREATE KEYSPACE IF NOT EXISTS cityinfo
-    WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}
-""")
+    #session.execute("""
+    #CREATE KEYSPACE IF NOT EXISTS cityinfo
+    #WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}""")
     session.execute('USE cityinfo')
-    session.execute('CREATE TABLE prova (id int,PRIMARY KEY(id))')
+    session.execute('CREATE TABLE IF NOT EXISTS prova (id int,PRIMARY KEY(id))')
     rows = session.execute('INSERT INTO prova (id) VALUES (0)')
     rows = session.execute('SELECT * FROM prova')
 
