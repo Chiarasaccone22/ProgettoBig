d4c17267fbea5c03c6f55aa6d6a95ad7a378f074
MatteoIng <matteofoschi10@gmail.com>
2023-08-25 13:49:57 +0200

    1 implementazione js

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 from waitress import serve
 from py2neo import Graph
 import psycopg2
@@ -31,6 +31,11 @@ connessioneCassandra=Cluster(['cassandraDb'], port=9042).connect()
 
 connessioneDynamo= boto3.resource('dynamodb',endpoint_url='http://dynamoDbGUI:8000',region_name='us-east-1')
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'  # '*' consente a qualsiasi dominio di accedere, ma è possibile specificare un dominio specifico
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
 
 #apertura di default
 @app.route('/')
@@ -40,90 +45,98 @@ def index():
 
 
 #Gestione connessione Postgres
-@app.route('/connPostgres')
+@app.route('/connPostgres',methods=['GET'])
 def connPostgres():
 
     cursor = connessionePostgres.cursor()
     cursor.execute("SELECT * FROM posts")
     results = cursor.fetchall()
-
-    return render_template('index.html', posts=results)
+    return jsonify(results)
+    #return render_template('index.html', posts=results)
 
 
 # Gestione del caricamento dei dataset nel database Postgres
-@app.route('/caricamentoPostgres')
+@app.route('/caricamentoPostgres',methods=['GET'])
 def caricamentoPostgresDB():
     #carichiamo il database con i csv in caricamentoPos e facciamo una query select
     postgres=caricamentoPos.caricamentoPostgres(connessionePostgres)
     cursor = postgres.cursor()
     cursor.execute("SELECT * FROM posts")
     results = cursor.fetchall()
-    return render_template('index.html', posts=results)
+    return jsonify(results)
+    #return render_template('index.html', posts=results)
 
 
 # Gestione connessione Dynamo
-@app.route('/connDynamo')
+@app.route('/connDynamo',methods=['GET'])
 def connDynamo():
     #carichiamo il database con i csv in caricamentoPos e restituiamo tutte le tabelle
     dynamodb = connessioneDynamo
     tables = list(dynamodb.tables.all())
-    return render_template('index.html', posts=tables)
+    return jsonify(tables)
+    #return render_template('index.html', posts=tables)
 
 
 # Gestione del caricamento dei dataset nel database Dynamo Db
-@app.route('/caricamentoDynamo')
+@app.route('/caricamentoDynamo',methods=['GET'])
 def caricamentoDynamoDB():
     dynamodb=caricamentoDy.caricamentoDynamo(connessioneDynamo)
     tables = list(dynamodb.tables.all())
-    return render_template('index.html', posts=tables)
+    return jsonify(tables)
+    #return render_template('index.html', posts=tables)
 
 # Gestione connessione Mongo
-@app.route('/connMongo')
+@app.route('/connMongo',methods=['GET'])
 def connMongo():
     mongo = connessioneMongo
     lista = mongo.list_database_names()
-    return render_template('index.html',posts=lista)
+    return jsonify(lista)
+    #return render_template('index.html',posts=lista)
 
 
 # Gestione del caricamento dei dataset nel database Mongo
-@app.route('/caricamentoMongo')
+@app.route('/caricamentoMongo',methods=['GET'])
 def caricamentoMongoDB():
     # Carichiamo il database con i csv in caricamentoMongo e restituiamo tutti i nomi delle tabelle
     mongo = caricamentoMongo.caricamentoMon(connessioneMongo)
     mongo = connessioneMongo
     lista = mongo.list_database_names()
-    return render_template('index.html',posts=lista)
+    return jsonify(lista)
+    #return render_template('index.html',posts=lista)
 
 
 # Gestione connessione Neo4j
-@app.route('/connNeo')
+@app.route('/connNeo',methods=['GET'])
 def connNeo():
     #connessione
     graph = connessioneNeo
     #query
     query = "MATCH (p:Person) RETURN p"
     result = graph.run(query)
-    return render_template('index.html', posts=result)
+    return jsonify(result)
+    #return render_template('index.html', posts=result)
 
 
 # Gestione connessione Cassandra
-@app.route('/connCassandra')
+@app.route('/connCassandra',methods=['GET'])
 def connCassandra():
     session = connessioneCassandra
     session.execute('USE cityinfo')
     session.execute('CREATE TABLE IF NOT EXISTS prova (id text,campo text,PRIMARY KEY(id))')
     rows = session.execute('SELECT * FROM prova')
-    return render_template('index.html', posts=rows)
+    return jsonify(rows)
+    #return render_template('index.html', posts=rows)
 
 
 # Gestione del caricamento dei dataset nel database Cassandra
-@app.route('/caricamentoCassandra')
+@app.route('/caricamentoCassandra',methods=['GET'])
 def caricamentoCassandraDB():
    # Carichiamo il database con i csv in caricamentoCassandra e restituiamo tutte le ennuple
     cassandra = caricamentoCassandra.caricamentodb(connessioneCassandra)
     cassandra.execute('USE cityinfo')
     rows = cassandra.execute('SELECT * FROM prova')
-    return render_template('index.html', posts=rows)
+    return jsonify(rows)
+    #return render_template('index.html', posts=rows)
 
 
 
