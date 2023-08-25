eda0b4cb30c9e86492861e3f0f3c11f6fe2f8c8d
MatteoIng <matteofoschi10@gmail.com>
2023-08-25 17:12:24 +0200

    pagina Html

--- flask_app/script/script.js
+++ flask_app/script/script.js
@@ -6,21 +6,21 @@ function prova(){
     // Esegui lo script al click del bottone
 
     // carico menu a tendina 1
-    /* const outputElement = document.getElementById('menu1');
-    const response = fetch(`http://localhost:8080/connPostgres`, { method: 'GET',
-                                                                    headers: { 'Accept': 'application/json',},
-    }).then(response => response.json()).then(dataArray => { 
-    for (i in dataArray){
-        console.log(dataArray[i])
-        var paragraph = document.createElement("option");
-        paragraph.textContent= dataArray[i]
-        outputElement.appendChild(paragraph)
-        //outputElement.textContent += `Risultato dello script: ${dataArray[i]}`;
-    }
-    })  */
+    const outputElement = document.getElementById('menu1');
+      const response = fetch(`http://localhost:8080/connPostgres`, { method: 'GET',
+                                                                      headers: { 'Accept': 'application/json',},
+      }).then(response => response.json()).then(dataArray => { 
+       for (i in dataArray){
+          console.log(dataArray[i])
+          var paragraph = document.createElement("option");
+          paragraph.textContent= dataArray[i]
+          outputElement.appendChild(paragraph)
+          //outputElement.textContent += `Risultato dello script: ${dataArray[i]}`;
+      }
+    })  
 
     // carico menu a tendina 2
-    const outputElement = document.getElementById('menu2');
+    /*const outputElement = document.getElementById('menu2');
     const response = fetch(`http://localhost:8080/connMongo`, { method: 'GET',
                                                                     headers: { 'Accept': 'application/json',},
     }).then(response => {response.json()}).then(dataArray => {
@@ -32,8 +32,8 @@ function prova(){
         paragraph.textContent= dataArray[i]
         outputElement.appendChild(paragraph)
         //outputElement.textContent += `Risultato dello script: ${dataArray[i]}`;
-    } */
-    })
+    } 
+    })*/
     //});
 }
 
